be6c14496842b9b2e2d6b80a8faae9611802e464
LeeHanJo <gps95wwkhhev@gmail.com>
2024-03-20 18:13:58 +0900

    - first commit -

---
+++ lib/group/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: TabBarScreen(),
+    );
+  }
+}
+
+class TabBarScreen extends StatefulWidget {
+  const TabBarScreen({Key? key}) : super(key: key);
+
+  @override
+  State<TabBarScreen> createState() => _TabBarScreenState();
+}
+
+class _TabBarScreenState extends State<TabBarScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController tabController = TabController(
+    length: 2,
+    vsync: this,
+    initialIndex: 0,
+
+    /// 탭 변경 애니메이션 시간
+    animationDuration: const Duration(milliseconds: 800),
+  );
+
+  @override
+  void dispose() {
+    tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("SynccIT"),
+      ),
+      body: _tabBar(),
+    );
+  }
+
+  Widget _tabBar() {
+    return TabBar(
+      controller: tabController,
+      tabs: const [
+        Tab(text: "공유 목록"),
+        Tab(text: "연락처 목록"),
+      ],
+    );
+  }
+}
